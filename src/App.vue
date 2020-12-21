157ec8d551f3f64cda6a0efc2cca41b454fcd032
dfxxxdf <137421378@qq.com>
2020-12-21 17:38:04 +0800

    '提交App.vue代码'

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,7 @@ export default {
 }
 </script>
 <style>
+  /*我们所创建的E:\phpstudy_pro\WWW\Vuejs\webpack\supermall\src\assets\css\base.css文件要怎么引用*/
+  /*1、使用以下的代码引用就可以了*/
+  @import "./assets/css/base.css";
 </style>
